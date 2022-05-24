50328ec5604b83ce76d5568cc7cdc8f04846db76
gosiamich <gosiamichalak@poczta.fm>
2022-05-24 21:58:30 +0200

    user signup commit

--- fundraiser_app/forms.py
+++ fundraiser_app/forms.py
@@ -12,16 +12,3 @@ class AddDonationModelForm(forms.ModelForm):
         model = Donation
         fields = ['quantity','categories', 'institution',\
         'address_street_no','city','phone_number','zip_code','pick_up_date','pick_up_time','pick_up_comment','user']
-
-#
-# class AddDonationForm(forms.Form):
-#     quantity = forms.IntegerField()
-#     institution = forms.ModelChoiceField(queryset=Institution.objects.all())
-#     address_street_no = forms.CharField(max_length=150)
-#     city = forms.CharField(max_length=64)
-#     zip_code = forms.CharField(max_length=7)
-#     phone_number = forms.CharField(max_length=50)
-#     pick_up_date = forms.DateField()
-#     pick_up_time = forms.TimeField()
-#     categories = forms.ModelMultipleChoiceField(queryset=Category.objects.all())
-#     pick_up_comment = forms.CharField(required=False)
